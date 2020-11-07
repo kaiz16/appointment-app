5923539acd9c6c9a588a6a746edd002b0b3e78e1
DJCarmen <carmenchoo89@me.com>
2020-11-07 21:00:38 +0800

    Added setting

---
+++ src/views/Setting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="title">Setting events in here</p>
+    <b-field label="Class Name">
+      <b-input v-model="name" placeholder="Type Class Name"></b-input>
+    </b-field>
+    <b-field label="Description">
+      <b-input maxlength="200" type="textarea" placeholder="Your appointment form description"></b-input>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  }
+};
+</script>
+
+<style>
+</style>
