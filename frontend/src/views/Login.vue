0c0d83ff777b362eaec7bc623a902f98ef6d494d
kaiz16 <realkaiz7@gmail.com>
2020-11-07 20:41:48 +0800

    Added login.vue

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <b-field label="Username"
+                type="is-success"
+                message="This username is available">
+                <b-input 
+                    v-model="username"
+                    value="johnsilver" maxlength="30"></b-input>
+            </b-field>
+
+            <b-field label="Password">
+                <b-input type="password"
+                    value="iwantmytreasure"
+                    v-model="password"
+                    password-reveal>
+                </b-input>
+            </b-field>
+
+            <b-button v-on:click="login">Submit</b-button>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login(){
+            console.log('logged in', this.username, this.password)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
