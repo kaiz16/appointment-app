6e917af317a3f5235e77d1c724f1ee3872c7f728
DJCarmen <carmenchoo89@me.com>
2020-11-15 18:42:05 +0800

    Login Page Text Field Shorten and Centered

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,52 @@
 <template>
   <section>
-     <b-field label="Email">
-        <b-input v-model="email" value="johnsilver" maxlength="30"></b-input>
-      </b-field>
+    <div class="mr-0 pt-5">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <b-field label="Email">
+            <b-input v-model="email" value="johnsilver" maxlength="30"></b-input>
+          </b-field>
 
-      <b-field label="Password">
-          <b-input type="password"
-              v-model="password"
-              password-reveal>
-          </b-input>
-      </b-field>
-    
-    <b-button @click="login">Submit</b-button>
+          <b-field label="Password">
+            <b-input type="password" v-model="password" password-reveal></b-input>
+          </b-field>
+
+          <b-button @click="login">Submit</b-button>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  data(){
+  data() {
     return {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   },
   methods: {
-    async login(){
-      const { data, error } = await axios({ 
-            url: 'auth/login', 
-            data: {
-              email: this.email,
-              password: this.password
-            },
-            method: 'POST'
-        })
-      if (error){
-        this.$buefy.toast.open('Error')
-      }else{
-        localStorage.setItem('auth-token', data.data.token)
-        this.$router.push('/dashboard')
+    async login() {
+      const { data, error } = await axios({
+        url: "auth/login",
+        data: {
+          email: this.email,
+          password: this.password
+        },
+        method: "POST"
+      });
+      if (error) {
+        this.$buefy.toast.open("Error");
+      } else {
+        localStorage.setItem("auth-token", data.data.token);
+        this.$router.push("/dashboard");
       }
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
