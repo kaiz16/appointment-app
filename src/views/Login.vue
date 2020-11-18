68707b77a76141913cad655fd230fe2f8dfcb64f
marcus68ganesha <marcusdave68@gmail.com>
2020-11-18 20:21:39 +0800

    login updated

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,53 @@
 <template>
-  <section>
+<section>
     <div class="mr-0 pt-5">
-      <div class="columns is-centered">
-        <div class="column is-half">
-          <b-field label="Email">
-            <b-input v-model="email" value="johnsilver" maxlength="30"></b-input>
-          </b-field>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <b-field label="Email">
+                    <b-input v-model="email" value="johnsilver" maxlength="30"></b-input>
+                </b-field>
 
-          <b-field label="Password">
-            <b-input type="password" v-model="password" password-reveal></b-input>
-          </b-field>
+                <b-field label="Password">
+                    <b-input type="password" v-model="password" password-reveal></b-input>
+                </b-field>
 
-          <b-button @click="login">Submit</b-button>
+                <b-button @click="login">Submit</b-button>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data() {
-    return {
-      email: "",
-      password: ""
-    };
-  },
-  methods: {
-    async login() {
-      const { data, error } = await axios({
-        url: "auth/login",
-        data: {
-          email: this.email,
-          password: this.password
-        },
-        method: "POST"
-      });
-      if (error) {
-        this.$buefy.toast.open("Error");
-      } else {
-        localStorage.setItem("auth-token", data.data.token);
-        this.$router.push("/dashboard");
-      }
+    data() {
+        return {
+            email: "",
+            password: ""
+        };
+    },
+    methods: {
+        async login() {
+            const {
+                data,
+                error
+            } = await axios({
+                url: "auth/login",
+                data: {
+                    email: this.email,
+                    password: this.password
+                },
+                method: "POST"
+            });
+            if (error) {
+                this.$buefy.toast.open("Error");
+            } else {
+                localStorage.setItem("auth-token", data.data.token);
+                this.$router.push("/dashboard");
+            }
+        }
     }
-  }
 };
 </script>
 
