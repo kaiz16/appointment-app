eb51da840cc7c14315f316ac84565b6d7c675ca4
DJCarmen <carmenchoo89@me.com>
2020-11-26 20:15:02 +0800

    Dashboard add new event validation & promt

--- src/components/organisms/NewEventForm.vue
+++ src/components/organisms/NewEventForm.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="card">
-      <div class="card-content">
-          <section>
-            <b-field label="Title">
-                <b-input v-model="title" value="johnsilver" maxlength="30"></b-input>
-            </b-field>
-            
-            <b-button @click="submit">Submit</b-button>
-        </section>
-      </div>
+    <div class="card-content">
+      <section>
+        <b-field label="Title">
+          <b-input v-model="title" placeholder="Your Event Title" maxlength="30" required></b-input>
+        </b-field>
+
+        <b-button @click="submit">Submit</b-button>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            title: ''
-        }
-    },
-    methods: {
-        submit(){
-            this.$emit('close', this.title)
-        }
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    submit() {
+      if (this.title == "" || this.title == null) {
+        this.$buefy.toast.open({
+          message: "<b>Title Can't Be Empty</b>",
+          type: "is-danger",
+          position: "is-bottom"
+        });
+      } else {
+        this.$emit("close", this.title);
+      }
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
